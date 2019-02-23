f93c1a526ba46c5bdd7537f6724d24a16be1c3e2
y2k <git@y2kbugger.com>
2019-02-22 23:48:29 -0500

    Minimal working example of MMIX REPL

--- Makefile
+++ Makefile
@@ -1,2 +1,36 @@
-all:
-	emcc mmix-wasm.c ./src/mmixlib/lib*.c ./src/mmixlib/mmix-arith.c ./src/mmixlib/mmix-io.c -s WASM=1 -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall', 'cwrap', 'FS_createDataFile', 'FS']" -o mmix.wasm  && emrun --no_browser --port 8080 .
+MMIXWARE=./mmixlib/mmixware
+MMIXLIB=./mmixlib
+MMIXLIB_SOURCES=./mmixlib/lib*.c ./mmixlib/mmix-arith.c ./mmixlib/mmix-io.c
+
+SOURCES=mmix-wasm.c
+
+EMSDK_ENV=source ./emsdk/emsdk_env.sh
+EMCC=$(EMSDK_ENV) && emcc
+CFLAGS=-Wno-extra-tokens
+
+all : emsdk mmix.wasm mmix.js mmix.js
+
+$(MMIXLIB_SOURCES) :
+	cd $(MMIXLIB) && make
+
+mmix.wasm mmix.js: $(MMIXLIB_SOURCES) $(SOURCES)
+	$(EMCC) $(SOURCES) $(MMIXLIB_SOURCES) $(CFLAGS) -s WASM=1 -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall', 'cwrap', 'FS_createDataFile', 'FS']" --emrun -o mmix.js
+	echo run a local server like this: 'emrun --no_browser --port 8080 .'
+
+emsdk :
+	git clone https://github.com/emscripten-core/emsdk.git
+	./emsdk/emsdk install latest
+	./emsdk/emsdk activate latest
+
+clean:
+	rm -f mmix.wasm
+	rm -f mmix.js
+	rm -f $(MMIXLIB)/abstime.h
+	rm -f $(MMIXLIB)/boilerplate.w
+	rm -f $(MMIXWARE)/abstime
+	cd $(MMIXLIB) && make clean
+	cd $(MMIXWARE) && make clean
+
+clean-emsdk:
+	rm -rf ./emsdk
+
--- mmix-wasm.c
+++ mmix-wasm.c
@@ -1,80 +1,84 @@
 #include "emscripten.h"
-#include "src/mmixlib/abstime.h"
+#include "mmixlib/abstime.h"
 #include <setjmp.h>
-//#include "src/mmixlib/libconfig.h"
-#include "src/mmixlib/libtype.h"
-#include "src/mmixlib/libglobals.h"
-#include "src/mmixlib/mmixlib.h"
+#include "mmixlib/libconfig.h"
+#include "mmixlib/libtype.h"
+#include "mmixlib/libglobals.h"
+#include "mmixlib/mmixlib.h"
 
 EMSCRIPTEN_KEEPALIVE
 int mmixal_wasm() {
-    return mmixal("in.mms", "out.mmo", "list.mml");
+    return mmixal("y2k.mms", "y2k.mmo", "y2k.mml");
 }
 
 EMSCRIPTEN_KEEPALIVE
-int mmix_wasm(int boot_argc, const char *argv[]) {
-    char *boot_cur_arg;
-    int argc;
-    puts(argv[0]);
+int mmix_wasm(int argc, char*argv[])
+{
+/* char**boot_cur_arg; */
+/* int boot_argc; */
+mmix_lib_initialize();
+g[255].h= 0;
+g[255].l= setjmp(mmix_exit);
+if(g[255].l!=0)
+goto end_simulation;
+/*170:*/
+#line 2930 "./mmixware/mmix-sim.w"
 
+/* myself= argv[0]; */
+/* for(cur_arg= argv+1;*cur_arg&&(*cur_arg)[0]=='-';cur_arg++) */
+/* scan_option(*cur_arg+1,true); */
+#line 2452 "./mmixlib.ch"
+MMIX_NO_FILE;
+/* argc-= (int)(cur_arg-argv); */
+#line 2936 "./mmixware/mmix-sim.w"
 
-    mmix_lib_initialize();
-    g[255].h= 0;
-    g[255].l= setjmp(mmix_exit);
-    if(g[255].l!=0)
-    goto end_simulation;
-    /*170:*/
-    //#line 2930 "./mmixware/mmix-sim.w"
+/*:170*/
+#line 2393 "./mmixlib.ch"
+;
 
-    myself= argv[0];
-    // for(cur_arg= argv+1;*cur_arg&&(*cur_arg)[0]=='-';cur_arg++)
-    //scan_option(*cur_arg+1,true);
-    //#line 2423 "./mmixlib.ch"
-    //# MMIX_NO_FILE;
-    //argc-= (int)(cur_arg-argv);
+mmix_initialize();
 
-    mmix_initialize();
+/* boot_cur_arg= cur_arg; */
+/* boot_argc= argc; */
+boot:
+/* argc= boot_argc; */
+/* cur_arg= boot_cur_arg; */
 
-    // All of the args are for the mmix program
-    boot_cur_arg= argv+1;
+mmix_boot();
 
-    boot:
-    argc= boot_argc;
-    //cur_arg= boot_cur_arg;
-
-    mmix_boot();
-
-    mmix_load_file("out.mmo");
-    mmix_commandline(argc,argv);
-    breakpoint= 0;
-    if(interacting&&!mmix_interact())goto end_simulation;
-    while(true){
-    if(interrupt&&!breakpoint)breakpoint|= trace_bit,interacting= true,interrupt= false;
-    else
-    #ifdef MMIX_TRAP
-    if(!(inst_ptr.h&sign_bit)||show_operating_system)
-    #endif
-    {breakpoint= 0;
-    if(interacting&&!mmix_interact())goto end_simulation;
-    }
-    if(halted)break;
-    do
-    {if(!resuming)
-    mmix_fetch_instruction();
-    mmix_perform_instruction();
-    mmix_trace();
-    mmix_dynamic_trap();
-    if(resuming&&op!=RESUME)resuming= false;
-    }while(resuming||(!interrupt&&!breakpoint));
-    if(interact_after_break)
-    interacting= true,interact_after_break= false;
-    if(g[rQ].l&g[rK].l&RE_BIT)
-    {breakpoint|= trace_bit;
-    goto boot;
-    }
-    }
-    end_simulation:if(profiling)mmix_profile();
-    if(interacting||profiling||showing_stats)show_stats(true);
-    mmix_finalize();
-    return g[255].l;
+/* mmix_load_file(*cur_arg); */
+mmix_load_file("y2k.mmo");
+/* mmix_commandline(argc,argv); */
+breakpoint= 0;
+if(interacting)mmix_interact();
+while(true){
+if(interrupt&&!breakpoint)breakpoint|= trace_bit,interacting= true,interrupt= false;
+else
+#ifdef MMIX_TRAP
+if(!(inst_ptr.h&sign_bit)||show_operating_system)
+#endif
+{breakpoint= 0;
+if(interacting)mmix_interact();
 }
+if(halted)break;
+do
+{if(!resuming)
+mmix_fetch_instruction();
+mmix_perform_instruction();
+mmix_trace();
+mmix_dynamic_trap();
+if(resuming&&op!=RESUME)resuming= false;
+}while(resuming||(!interrupt&&!breakpoint));
+if(interact_after_break)
+interacting= true,interact_after_break= false;
+if(g[rQ].l&g[rK].l&RE_BIT)
+{breakpoint|= trace_bit;
+goto boot;
+}
+}
+end_simulation:if(profiling)mmix_profile();
+if(interacting||profiling||showing_stats)show_stats(true);
+mmix_finalize();
+return g[255].l;
+}
+#line 2918 "./mmixware/mmix-sim.w"
