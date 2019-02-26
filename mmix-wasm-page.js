751cd048d05d8e6c3067172d17ba2119a44967f2
y2k <git@y2kbugger.com>
2019-02-26 00:50:55 -0500

    Both js main loop and native main loop working

--- mmix-wasm-page.js
+++ mmix-wasm-page.js
@@ -1,13 +1,14 @@
-initialMixalProgram = `argv   IS    $1
-       #BYTE "UUUaaa",99,99,99,99,99,99
-       LOC   #100
-Main   LDOU  $255,argv,0
-       TRAP  0,Fputs,StdOut
-       GETA  $255,String
-       TRAP  0,Fputs,StdOut
-       TRAP  0,Halt,0
-String BYTE  ", world",#a,0
-`;
+initialMixalProgram = `	LOC	#20
+	GREG	@
+argv	BYTE	"Hello,",10
+	LOC	#100
+	GREG	@
+Main	LDA	$255,argv
+	TRAP	0,Fputs,StdOut
+	GETA	$255,String
+	TRAP	0,Fputs,StdOut
+	TRAP	0,Halt,0
+String	BYTE	"	World",#a,0`;
 
 function makeWriter(elementid) {
     var element = document.getElementById(elementid);
@@ -37,81 +38,72 @@ function rerun(){
     mmixal();
     if (document.getElementById('error').value == "")
     {
-        mmix_sim(1, "doesn't make diff.mmo");
-        // mmix_sim_js()
+        // mmix_sim();
+        mmix_sim_js()
     }
     rerunOldVal = inputElement.value;
 }
 
 function mmix_sim_js(){
-    // mmix_lib_initialize();
-    // mmix_initialize();
-    // boot:
-    // mmix_boot();
-    // mmix_load_file("y2k.mmo");
-    // breakpoint= 0;
-    // if(interacting)
-    // {
-    //     mmix_interact();
-    // }
-    // while(true){
-    //     if(interrupt&&!breakpoint)
-    //     {
-    //         breakpoint|= trace_bit,interacting= true,interrupt= false;
-    //     }
-    //     else
-    //     {
-    //         breakpoint= 0;
-    //         if(interacting)
-    //         {
-    //             mmix_interact();
-    //         }
-    //     }
-    //     if(halted)
-    //     {
-    //         break;
-    //     }
-    //     do
-    //     {
-    //         if(!resuming)
-    //             mmix_fetch_instruction();
-    //         mmix_perform_instruction();
-    //         mmix_trace();
-    //         mmix_dynamic_trap();
-    //         if(resuming&&op!=RESUME)
-    //             resuming= false;
-    //     }while(resuming||(!interrupt&&!breakpoint));
-    //     if(interact_after_break)
-    //         interacting= true,interact_after_break= false;
-    //         if(g[rQ].l&g[rK].l&RE_BIT)
-    //         {
-    //             breakpoint|= trace_bit;
-    //             goto boot;
-    //         }
-    // }
-    // end_simulation:
-    // if(profiling)
-    //     mmix_profile();
-    // if(interacting||profiling||showing_stats)
-    //     show_stats(true);
-    // mmix_finalize();
-
     mmix_lib_initialize();
     mmix_initialize();
     mmix_boot();
-    mmix_load_file("y2k.mmo");
+    y2k_mmix_load_file();
+
+    // set_breakpoint(0);
+    // if(get_interacting())
+    // {
+    //     mmix_interact();
+    // }
+
     while(true)
     {
-        console.log('interating');
-        if (halted())
+        console.log('start execution cycle');
+
+        // if(get_interrupt()&&!get_breakpoint())
+        // {
+        //     set_breakpoint(get_breakpoint() | get_trace_bit());
+        //     set_interacting(true);
+        //     set_interrupt(false);
+        // }
+        // else
+        // {
+        //     set_breakpoint(0);
+        //     if(get_interacting())
+        //     {
+        //         mmix_interact();
+        //     }
+        // }
+        if (get_halted())
         {
             console.log('halting');
             break;
         }
-        mmix_fetch_instruction();
-        mmix_perform_instruction();
-        mmix_trace();
-        mmix_dynamic_trap();
+
+        do
+        {
+            console.log('Inner looper');
+            if(!get_resuming())
+            {
+                mmix_fetch_instruction();
+            }
+            mmix_perform_instruction();
+            // mmix_trace();
+            mmix_dynamic_trap();
+            // if(get_resuming() && op != RESUME)
+            if(get_resuming())
+            {
+                set_resuming(false);
+            }
+
+
+        } while(get_resuming()||(!get_interrupt()&&!get_breakpoint()));
+
+        // if(get_interact_after_break())
+        // {
+        //     set_interacting(true);
+        //     set_interact_after_break(false);
+        // }
     }
     show_stats(true);
     mmix_finalize();
@@ -121,16 +113,18 @@ Module.print = makeWriter('output');
 Module.printErr = makeWriter('error');
 Module.onRuntimeInitialized = function (){
     // wait until runtimes are loaded to map these
-    mmix_sim = Module.cwrap('mmix_sim', 'number', ['number', 'array']);
+    mmix_sim = Module.cwrap('mmix_sim', 'number', []);
     mmixal = Module.cwrap('mmixal_wasm', 'number', []);
     halted = Module.cwrap('halted_wasm', 'number', []);
     print_mystr = Module.cwrap('print_mystr', 'number', ['string']);
     mmix_initialize_globals = Module.cwrap('mmix_initialize_globals', 'number', []);
+    y2k_mmix_load_file = Module.cwrap('y2k_mmix_load_file', null, []);
 
     mmix_lib_initialize = Module.cwrap('mmix_lib_initialize', 'number', []);
     mmix_initialize = Module.cwrap('mmix_initialize', 'number', []);
     mmix_boot = Module.cwrap('mmix_boot', 'number', []);
     mmix_load_file = Module.cwrap('mmix_boot', 'number', ['string']);
+    mmix_interact = Module.cwrap('mmix_interact', 'number', ['string']);
 
     mmix_fetch_instruction = Module.cwrap('mmix_fetch_instruction', 'number', []);
     mmix_perform_instruction = Module.cwrap('mmix_perform_instruction', 'number', []);
@@ -139,9 +133,45 @@ Module.onRuntimeInitialized = function (){
     show_stats = Module.cwrap('show_stats', 'number', []);
     mmix_finalize = Module.cwrap('mmix_finalize', 'number', []);
 
+
+get_halted = Module.cwrap('get_halted', 'bool', []);
+set_halted = Module.cwrap('set_halted', null, ['bool']);
+
+get_breakpoint = Module.cwrap('get_breakpoint', 'number', []);
+set_breakpoint = Module.cwrap('set_breakpoint', null, ['number']);
+
+get_interrupt = Module.cwrap('get_interrupt', 'bool', []);
+set_interrupt = Module.cwrap('set_interrupt', null, ['bool']);
+
+get_resuming = Module.cwrap('get_resuming', 'bool', []);
+set_resuming = Module.cwrap('set_resuming', null, ['bool']);
+
+get_interacting = Module.cwrap('get_interacting', 'bool', []);
+set_interacting = Module.cwrap('set_interacting', null, ['bool']);
+
+get_interact_after_break = Module.cwrap('get_interact_after_break', 'bool', []);
+set_interact_after_break = Module.cwrap('set_interact_after_break', null, ['bool']);
+
+get_profiling = Module.cwrap('get_profiling', 'bool', []);
+set_profiling = Module.cwrap('set_profiling', null, ['bool']);
+
+get_showing_stats = Module.cwrap('get_showing_stat', 'bool', []);
+set_showing_stats = Module.cwrap('set_showing_stat', null, ['bool']);
+
+get_trace_bit = Module.cwrap('get_trace_bit', 'number', []);
+
     inputElement.value = initialMixalProgram;
+    rerun();
 }
 $("#input").on('keyup paste', rerun);
+document.getElementById("input").addEventListener("keydown", function(e) {
+    if (e.which===9) {
+        e.preventDefault();
+        document.execCommand("insertText", false, "\t");
+    }
+}, false);
+
+// https://stackoverflow.com/questions/6637341/use-tab-to-indent-in-textarea
 
 // // Create a Web Worker (separate thread) that we'll pass the WebAssembly module to.
 //  var g_WebWorker = new Worker("webworker.js");
--- mmix-wasm.c
+++ mmix-wasm.c
@@ -12,44 +12,33 @@ EMSCRIPTEN_KEEPALIVE
 int mmixal_wasm() {
     return mmixal("y2k.mms", "y2k.mmo", "y2k.mml");
 }
-EMSCRIPTEN_KEEPALIVE
-bool get_halted() {
-    return halted;
-}
-EMSCRIPTEN_KEEPALIVE
-void set_halted(bool v) {
-    halted = v;
-}
+
+EMSCRIPTEN_KEEPALIVE bool get_halted() { return halted; }
+EMSCRIPTEN_KEEPALIVE void set_halted(bool v) { halted = v; }
+
+EMSCRIPTEN_KEEPALIVE int get_breakpoint() { return breakpoint; }
+EMSCRIPTEN_KEEPALIVE void set_breakpoint(int v) { breakpoint = v; }
+
+EMSCRIPTEN_KEEPALIVE bool get_interrupt() { return interrupt; }
+EMSCRIPTEN_KEEPALIVE void set_interrupt(bool v) { interrupt = v; }
+
+EMSCRIPTEN_KEEPALIVE bool get_resuming() { return resuming; }
+EMSCRIPTEN_KEEPALIVE void set_resuming(bool v) { resuming = v; }
+
+EMSCRIPTEN_KEEPALIVE bool get_interacting() { return interacting; }
+EMSCRIPTEN_KEEPALIVE void set_interacting(bool v) { interacting = v; }
+
+EMSCRIPTEN_KEEPALIVE bool get_interact_after_break() { return interact_after_break; }
+EMSCRIPTEN_KEEPALIVE void set_interact_after_break(bool v) { interact_after_break = v; }
+
+EMSCRIPTEN_KEEPALIVE bool get_profiling() { return profiling; }
+EMSCRIPTEN_KEEPALIVE void set_profiling(bool v) { profiling = v; }
+
+EMSCRIPTEN_KEEPALIVE bool get_showing_stats() { return showing_stats; }
+EMSCRIPTEN_KEEPALIVE void set_showing_stats(bool v) { showing_stats = v; }
 
 EMSCRIPTEN_KEEPALIVE
-int get_breakpoint() {
-    return breakpoint;
-}
-EMSCRIPTEN_KEEPALIVE
-void set_breakpoint(int v) {
-    breakpoint = v;
-}
-
-EMSCRIPTEN_KEEPALIVE
-bool get_interrupt() {
-    return interrupt;
-}
-EMSCRIPTEN_KEEPALIVE
-void set_interrupt(bool v) {
-    interrupt = v;
-}
-
-EMSCRIPTEN_KEEPALIVE
-bool get_resuming() {
-    return resuming;
-}
-EMSCRIPTEN_KEEPALIVE
-void set_resuming(bool v) {
-    resuming = v;
-}
-
-EMSCRIPTEN_KEEPALIVE
-bool get_trace_bit() {
+int get_trace_bit() {
     // this is actually a preprocessor builtin
     // defined bitmask (1<<3)
     // Therefor, no setter
@@ -64,74 +53,75 @@ int print_mystr(char*mystr)
 }
 
 EMSCRIPTEN_KEEPALIVE
-int mmix_sim(int argc, char*argv[])
+void y2k_mmix_load_file(){
+    mmix_load_file("y2k.mmo");
+}
+
+EMSCRIPTEN_KEEPALIVE
+int mmix_sim()
 {
-
-/* char**boot_cur_arg; */
-/* int boot_argc; */
-mmix_lib_initialize();
-g[255].h= 0;
-g[255].l= setjmp(mmix_exit);
-if(g[255].l!=0)
-goto end_simulation;
-/*170:*/
-#line 2930 "./mmixware/mmix-sim.w"
-
-/* myself= argv[0]; */
-/* for(cur_arg= argv+1;*cur_arg&&(*cur_arg)[0]=='-';cur_arg++) */
-/* scan_option(*cur_arg+1,true); */
-#line 2452 "./mmixlib.ch"
-MMIX_NO_FILE;
-/* argc-= (int)(cur_arg-argv); */
-#line 2936 "./mmixware/mmix-sim.w"
-
-/*:170*/
-#line 2393 "./mmixlib.ch"
-;
-mmix_initialize();
-
-/* boot_cur_arg= cur_arg; */
-/* boot_argc= argc; */
+    mmix_lib_initialize();
+    /* g[255].h= 0; */
+    /* g[255].l= 0; */
+    mmix_initialize();
 boot:
-/* argc= boot_argc; */
-/* cur_arg= boot_cur_arg; */
+    mmix_boot();
+    mmix_load_file("y2k.mmo");
+    /* mmix_commandline(argc,argv); */
 
-mmix_boot();
+    /* set_breakpoint(0); */
+    /* if(get_interacting()) */
+    /*     mmix_interact(); */
 
-/* mmix_load_file(*cur_arg); */
-mmix_load_file("y2k.mmo");
-/* mmix_commandline(argc,argv); */
-breakpoint= 0;
-if(interacting)mmix_interact();
-while(true){
-if(interrupt&&!breakpoint)breakpoint|= trace_bit,interacting= true,interrupt= false;
-else
-#ifdef MMIX_TRAP
-if(!(inst_ptr.h&sign_bit)||show_operating_system)
-#endif
-{breakpoint= 0;
-if(interacting)mmix_interact();
-}
-if(halted)break;
-do
-{if(!resuming)
-mmix_fetch_instruction();
-mmix_perform_instruction();
-mmix_trace();
-mmix_dynamic_trap();
-if(resuming&&op!=RESUME)resuming= false;
-}while(resuming||(!interrupt&&!breakpoint));
-if(interact_after_break)
-interacting= true,interact_after_break= false;
-if(g[rQ].l&g[rK].l&RE_BIT)
-{breakpoint|= trace_bit;
-goto boot;
-}
-}
-end_simulation:if(profiling)mmix_profile();
-if(interacting||profiling||showing_stats)show_stats(true);
-show_stats(true);
-mmix_finalize();
-return g[255].l;
+    while(true){
+        /* if(get_interrupt()&&!get_breakpoint()) */
+        /* { */
+        /*     set_breakpoint(breakpoint | trace_bit); */
+        /*     set_interacting(true); */
+        /*     set_interrupt(false); */
+        /* } */
+        /* else */
+        /* { */
+        /*     set_breakpoint(0); */
+        /*     if(get_interacting()) */
+        /*         mmix_interact(); */
+        /* } */
+        if(get_halted())
+        {
+            printf("zasd");
+            break;
+        }
+
+        do
+        {
+            if(!get_resuming())
+                mmix_fetch_instruction();
+            mmix_perform_instruction();
+            mmix_trace();
+            mmix_dynamic_trap();
+            if(get_resuming() && op != RESUME)
+                set_resuming(false);
+        }
+        while(get_resuming()||(!get_interrupt()&&!get_breakpoint()));
+
+        /* if(get_interact_after_break()) */
+        /* { */
+        /*     set_interacting(true); */
+        /*     set_interact_after_break(false); */
+        /* } */
+        /* if(g[rQ].l&g[rK].l&RE_BIT) */
+        /* { */
+        /*     set_breakpoint(get_breakpoint() | get_trace_bit()); */
+        /*     goto boot; */
+        /* } */
+    }
+    end_simulation:
+    if(get_profiling())
+        mmix_profile();
+    if(get_interacting()||get_profiling()||get_showing_stats())
+        show_stats(true);
+    show_stats(true);
+    mmix_finalize();
+    return g[255].l;
 }
 #line 2918 "./mmixware/mmix-sim.w"
